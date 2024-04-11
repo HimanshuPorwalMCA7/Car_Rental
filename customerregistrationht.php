38ec5f941736aa27f53f168d46805648eadff7b5
HimanshuPorwalMCA7 <himanshuporwal7417@gmail.com>
2024-04-11 09:54:08 +0530

    fixed Bug

--- customerregistrationht.php
+++ customerregistrationht.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['customer_logged_in']) && $_SESSION['customer_logged_in'] ==
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="dashboard.html">Car Rental Services</a>
+        <a class="navbar-brand" href="index.php">Car Rental Services</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
